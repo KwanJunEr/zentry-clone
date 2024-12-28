207b4d5edc50dafb28adce648aff209f5f7ba4d7
KwanJunEr <jonastheitguy@gmail.com>
2024-12-28 16:07:53 +0800

    completed Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -8,7 +10,7 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
-  const totalVideos = 3;
+  const totalVideos = 4;
   const nextVideoRef = useRef(null); //useRef to target specific DOM elements
 
   const handleVideoLoaded = () => {
@@ -24,6 +26,30 @@ const Hero = () => {
     setHasClicked(true);
     setCurrentIndex(upcomingVideoIndex);
   };
+
+  useGSAP(()=>{
+    if(hasClicked){
+        gsap.set('#next-video', {visibility: 'visible'})
+
+        //Scaling and sizing animation 
+        gsap.to("#next-video",{
+            transformOrigin: 'center center', //scale from center
+            scale: 1,
+            width: '100%',
+            height: '100%',
+            duration : 1,
+            ease: 'power1.inOut',
+            onStart: ()=> nextVideoRef.current.play(),
+        })
+
+        gsap.from('current-video',{
+            transformOrigin: 'center center',
+            scale: 0,
+            duration: 1.5,
+            ease: "power1.inOut"
+        })
+    }
+  },{dependencies: [currentIndex ], revertOnUpdate: true})
 
   const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
